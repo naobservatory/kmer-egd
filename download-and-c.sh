595fb2630c5c7764a9b8ba5b5d834c3ce1a20b15
Jeff Kaufman <jeff@securebio.org>
2022-08-26 13:24:01 +0000

    polyg trimming, missing n_buckets

--- download-and-c.sh
+++ download-and-c.sh
@@ -1,4 +1,11 @@
-aws s3 cp s3://prjna729801/"$1" - \
+INPUT=($1)
+FNAME="${INPUT[0]}"
+SHM=shm-"${INPUT[1]}"
+N_BUCKETS=$2
+
+echo "Processing $FNAME on $SHM"
+aws s3 cp s3://prjna729801/"$FNAME" - \
     | gunzip \
-    | ~/kmer-egd/count-kmer-eqs "$2" \
+    | sed 's/GGGGGGG*$//' \
+    | ~/kmer-egd/count-kmer-eqs "$SHM" "$N_BUCKETS" \
     > /dev/null                   
