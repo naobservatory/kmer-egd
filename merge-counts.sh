ceb6aedf18538e956fe7380105682540dd4ef469
Jeff Kaufman <jeff@securebio.org>
2022-08-29 17:43:50 +0000

    use what data is finished processing

--- merge-counts.sh
+++ merge-counts.sh
@@ -1,7 +1,10 @@
 echo -n 'python3 merge-counts.py'
 
-cat prjna729801.fnames | while read fname; do
-    echo -n " <(aws s3 cp s3://prjna729801/$fname.counted_kmers.gz - | gunzip)"
-done
+aws s3 ls s3://prjna729801/ | \
+    grep counted_kmers.gz | \
+    awk '$3>1000000{print $NF}' | \
+    while read fname; do
+        echo -n " <(aws s3 cp s3://prjna729801/$fname - | gunzip)"
+    done
 
 # ./merge-counts.sh > tmp.merge-counts.sh && bash tmp.merge-counts.sh
