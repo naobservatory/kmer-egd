4f04b056fd7ac15be4fffb0c71a8d383c08cf97d
Jeff Kaufman <jeff@securebio.org>
2022-08-29 20:19:00 +0000

    tweak eval-buckets.py to handle the output of running ./merge-counts.sh

--- eval-buckets.py
+++ eval-buckets.py
@@ -4,20 +4,23 @@ from sklearn import linear_model
 
 def start():
     clf = linear_model.PoissonRegressor()
-    sums = None
+    #sums = None
     days = None
     for line in sys.stdin:
-        if not sums:
-            _, *sums = line.strip().split('\t')
-            sums = [int(x) for x in sums]
-            days = [[day] for day in range(len(sums))]
-            continue
+        #if not sums:
+        #    _, *sums = line.strip().split('\t')
+        #    sums = [int(x) for x in sums]
+        #    days = [[day] for day in range(len(sums))]
+        #    continue
 
         bucket, *vals = line.strip().split('\t')
         vals = [int(x) for x in vals]
-        
-        print(repr(vals))
 
+        if not days:
+            days = [[day] for day in range(len(vals))]
+        
+        #print(repr(vals))
+        
         # TODO: adjust by sums
         clf.fit(days, vals)
         print("%s\t%s\t%s" % (
