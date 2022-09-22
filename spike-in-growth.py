197943b0b2a70bfc5e42946f593a549db63e0f70
Jeff Kaufman <jeff@securebio.org>
2022-09-22 18:39:15 +0000

    instead of always rounding down, round randomly

--- spike-in-growth.py
+++ spike-in-growth.py
@@ -1,6 +1,17 @@
 import sys
+import random
+
+def randround(val):
+    ival = int(val)
+    delta = val - ival
+    if random.random() < delta:
+        return ival + 1
+    return ival
 
 def start(days, final, growth):
+    # Make the output deterministic
+    random.seed(hash((days, final, growth)))
+
     days = int(days)
     final = int(final)
     growth = 1+float(growth)
@@ -8,7 +19,7 @@ def start(days, final, growth):
     val = final
     out = []
     for _ in range(days):
-        out.append(str(int(val)))
+        out.append(str(randround(val)))
         val /= growth
     out.reverse()
         
