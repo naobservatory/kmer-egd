ab2697a44e53a8c8379e57809f5ac5e32d23cff1
Jeff Kaufman <jeff@securebio.org>
2022-09-22 16:11:18 +0000

    allow specifying the number of days and the specific values to test at

--- run-spike-in.sh
+++ run-spike-in.sh
@@ -1,11 +1,14 @@
-for final in 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 \
-                 1500 1600 1700 1800 1900 2000; do
+DAYS=$1
+shift
+VALS="$@"
+
+for final in $VALS; do
     echo -n $final
-    for growth in 05 07 10 15; do
+    for growth in .05 .07 .10 .15; do
         echo -n "" $(python3 virtual-spike-in.py \
                                       hiv.fasta \
                                       quality-counts.jsons $(
-            python3 spike-in-growth.py 14 $final .$growth) | \
+            python3 spike-in-growth.py $DAYS $final $growth) | \
                 python3 eval-buckets-sm.py | \
                 sort -g | \
                 head -n 1 | \
