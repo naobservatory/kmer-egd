33e5c80d39aba1ec9ef54c9aca98a0f32a37251c
Jeff Kaufman <jeff@securebio.org>
2022-11-03 19:01:16 +0000

    require specifying the number of buckets

--- make-cdf.py
+++ make-cdf.py
@@ -1,7 +1,8 @@
 import sys
-BUCKETS=1000
+buckets, = sys.argv[1:]
+buckets = int(buckets)
 
 data = [line.strip() for line in sys.stdin]
-for i in range(BUCKETS):
-    print("%s\t%.3f%%" % (data[i*len(data)//BUCKETS], i/BUCKETS*100))
+for i in range(buckets):
+    print("%s\t%.3f%%" % (data[i*len(data)//buckets], i/buckets*100))
     
