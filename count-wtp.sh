48fb3cbb1b7a19acf53de2afb5b533d9cba277b2
Jeff Kaufman <jeff@jefftk.com>
2022-11-10 15:14:12 -0500

    count-wtp: make idempotent

--- count-wtp.sh
+++ count-wtp.sh
@@ -1,12 +1,19 @@
-WTP="$1"
 for a in A C G T; do
     for b in A C G T; do
+        fname_out="s3://prjna729801/hc-HTP-$a$b.gz"
+
+        if aws s3 ls "$fname_out" > /dev/null ; then
+            echo "already done"
+            continue
+        fi
+        echo "would do $a$b"
+
         time cat rothman.unenriched.simple | \
-            awk -F '\t' '$NF=="'$WTP'"{print $1"_1.fastq.gz\n"$1"_2.fastq.gz"}' | \
+            awk -F '\t' '$NF=="HTP"{print $1"_1.fastq.gz\n"$1"_2.fastq.gz"}' | \
             xargs -P1 -I {} aws s3 cp s3://prjna729801/{} - | \
             gunzip | \
-            ./hash-count-rothman $WTP $a$b | \
+            ./hash-count-rothman HTP $a$b | \
             gzip | \
-            aws s3 cp - s3://prjna729801/hc-$WTP-$a$b.gz
+            aws s3 cp - "$fname_out"
     done
 done
