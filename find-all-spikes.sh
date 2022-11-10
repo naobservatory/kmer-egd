7fb4c2fdfa4fea1b9f1843b516e4556ed6a51e13
Jeff Kaufman <jeff@jefftk.com>
2022-11-10 15:14:00 -0500

    tidy: use continue

--- find-all-spikes.sh
+++ find-all-spikes.sh
@@ -4,15 +4,15 @@ for a in A C G T; do
         fname_out="s3://prjna729801/hc-HTP-$a$b.spikes.gz"
 
         if aws s3 ls "$fname_out" > /dev/null ; then
-            # already done
-        else
-            echo $a$b
-            aws s3 cp "$fname_in" - | \
-                gunzip | \
-                python3 find-spikes.py | \
-                gzip | \
-                aws s3 cp - "$fname_out" &
+            continue
         fi
+
+        echo $a$b
+        aws s3 cp "$fname_in" - | \
+            gunzip | \
+            python3 find-spikes.py | \
+            gzip | \
+            aws s3 cp - "$fname_out" &
     done
 done
 wait
