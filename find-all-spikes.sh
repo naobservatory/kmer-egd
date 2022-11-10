c9653ed1acd205c55e69867d897901c8312d85cc
Jeff Kaufman <jeff@jefftk.com>
2022-11-10 15:05:53 -0500

    find-all-spikes: make idempotent

--- find-all-spikes.sh
+++ find-all-spikes.sh
@@ -1,11 +1,18 @@
 for a in A C G T; do
     for b in A C G T; do
-        echo $a$b
-        aws s3 cp s3://prjna729801/hc-HTP-$a$b.gz - | \
-            gunzip | \
-            python3 find-spikes.py | \
-            gzip | \
-            aws s3 cp - s3://prjna729801/hc-HTP-$a$b.spikes.gz &
+        fname_in="s3://prjna729801/hc-HTP-$a$b.gz"
+        fname_out="s3://prjna729801/hc-HTP-$a$b.spikes.gz"
+
+        if aws s3 ls "$fname_out" > /dev/null ; then
+            # already done
+        else
+            echo $a$b
+            aws s3 cp "$fname_in" - | \
+                gunzip | \
+                python3 find-spikes.py | \
+                gzip | \
+                aws s3 cp - "$fname_out" &
+        fi
     done
 done
 wait
