311c65b33343ab146429d0bc6a1714b91cb76058
Jeff Kaufman <jeff@jefftk.com>
2022-11-10 15:47:44 -0500

    partition-spikes: refactor and add runner

--- partition-spikes.py
+++ partition-spikes.py
@@ -1,21 +1,10 @@
 import sys
 
-fname_in, = sys.argv[1:]
+day, = sys.argv[1:]
 
-days = []
+for line in sys.stdin:
+    biggest_count, bucket, counts = line.strip().split(sep="\t", maxsplit=2)
+    counts = counts.split("\t")
 
-with open(fname_in) as inf:
-    for line in inf:
-        biggest_count, bucket, counts = line.strip().split(sep="\t", maxsplit=2)
-        counts = counts.split("\t")
-
-        if not days:
-            for _ in range(len(counts)):
-                days.append([])
-
-        days[counts.index(biggest_count)].append((biggest_count, bucket))
-
-for i, day_data in enumerate(days):
-    with open("%s.%s" % (fname_in, str(i).zfill(2)), "w") as outf:
-        for biggest_count, bucket in day_data:
-            outf.write("%s\t%s\n" % ( biggest_count, bucket))
+    if counts.index(biggest_count) == int(day):
+        print("%s\t%s" % (biggest_count, bucket))
---
+++ partition-spikes.sh
@@ -0,0 +1,18 @@
+for day in {00..20}; do
+    fname_out="s3://prjna729801/hc-HTP.spikes.$day.gz"
+
+    if aws s3 ls "$fname_out" > /dev/null ; then
+        continue
+    fi
+
+    echo "$day..."
+
+    for a in A C G T ; do
+        for b in A C G T ; do
+            aws s3 cp s3://prjna729801/hc-HTP-$a$b.spikes.gz - | gunzip
+        done
+    done | \
+        python3 partition-spikes.py $day | \
+        gzip | \
+        aws s3 cp - $fname_out
+done
