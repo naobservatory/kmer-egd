7589e19e9efb607a98536e4baef7165bed2d07f7
Jeff Kaufman <jeff@securebio.org>
2022-11-25 02:19:09 +0000

    accidentally committed an old version; restore from 311c65b33343ab146429d0bc6a1714b91cb76058

--- partition-spikes.py
+++ partition-spikes.py
@@ -1,19 +1,10 @@
 import sys
 
-out_prefix, = sys.argv[1:]
+day, = sys.argv[1:]
 
 for line in sys.stdin:
     biggest_count, bucket, counts = line.strip().split(sep="\t", maxsplit=2)
     counts = counts.split("\t")
 
-    if not days:
-        for _ in range(len(counts)):
-            days.append([])
-
-    days[counts.index(biggest_count)].append((biggest_count, bucket))
-
-for i, day_data in enumerate(days):
-    with open("%s.%s" % (out_prefix, str(i).zfill(2)), "w") as outf:
-        for biggest_count, bucket in day_data:
-            outf.write("%s\t%s\n" % ( biggest_count, bucket))
-
+    if counts.index(biggest_count) == int(day):
+        print("%s\t%s" % (biggest_count, bucket))
