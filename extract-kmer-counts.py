ea53b321c7708f0f37028d85caefc4553344cb97
Jeff Kaufman <jeff@securebio.org>
2022-12-09 18:49:04 +0000

    extract kmers for both seq and its reverse complement

--- extract-kmer-counts.py
+++ extract-kmer-counts.py
@@ -7,13 +7,21 @@ K=40
 
 to_match = set()
 
+def rc(s):
+    return "".join({'T':'A',
+                    'G':'C',
+                    'A':'T',
+                    'C':'G',
+                    'N':'N'}[x] for x in reversed(s))
+
 with open(seq_fname) as inf:
     for line in inf:
         seq = line.strip()
         for i in range(len(seq) - K + 1):
-            kmer = seq[i:i+K]
-            if kmer.startswith(prefix):
-                to_match.add(kmer)
+            kmer_in = seq[i:i+K]
+            for kmer in [kmer_in, rc(kmer_in)]:
+                if kmer.startswith(prefix):
+                    to_match.add(kmer)
 
 for line in sys.stdin:
     if line.split()[0] in to_match:
