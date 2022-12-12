1e24f060c04b9d50984db765f4bb555c5aa3f68c
Jeff Kaufman <jeff@securebio.org>
2022-12-12 16:27:38 +0000

    find-aligning-reads: use raw reads, not cleaned data, so that: - we're sure we're getting the real start - we know whether it's from the forward or reverse read

--- find-aligning-reads.sh
+++ find-aligning-reads.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-for accession in $(cat rothman.unenriched.simple | \
-                       awk -F '\t' '$NF=="HTP"{print $1}'); do
-    aws s3 cp s3://prjna729801/$accession.arclean.fastq.gz - | \
+cat rothman.unenriched.simple | \
+    awk -F '\t' '$NF=="HTP"{print $1"_1"; print $1"_2"}' | \
+    xargs -P 8 -I {} bash -c "aws s3 cp s3://prjna729801/{}.fastq.gz - | \
         gunzip | \
+        sed s/CTGTCTCTTATACACATCT.*// | \
         ./find-aligning-reads.py NODE_82_length_6360_cov_146981.720834.seq > \
-                                 $accession.tbrv.alignment-points &
-done
-wait
+        {}.tbrv.alignment-points"
