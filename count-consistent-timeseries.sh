4377a53edd0fc406143dd1738f1ea7f78be40fe6
Jeff Kaufman <jeff@securebio.org>
2023-01-05 19:17:51 +0000

    check in kmer counting code

--- count-consistent-timeseries.sh
+++ count-consistent-timeseries.sh
@@ -1,21 +1,26 @@
+#!/bin/bash
+
 for wtp in $(cat longest-timeseries.tsv | awk '{print $3}' | sort | uniq); do
   for a in A C G T; do
     for b in A C G T; do
         fname_out="s3://prjna729801/clean-TS-$wtp-$a$b.gz"
 
-        if aws s3 ls "$fname_out" > /dev/null ; then
-            continue
-        fi
+        #if aws s3 ls "$fname_out" > /dev/null ; then
+        #    continue
+        #fi
 
-        echo "... $a$b"
+        echo "... $wtp $a$b"
         time cat longest-timeseries.tsv | \
-            awk -F '{print $1}' | \
-            xargs -P1 -I {} aws s3 cp s3://prjna729801/{} - | \
+            awk -F '\t' '$3=="'$wtp'"{print $1}' | \
+            xargs -P1 -I {} aws s3 cp s3://prjna729801/{}.arclean.fastq.gz - | \
             gunzip | \
             sed -E 's/^@MT?_/@/' | \
-            ./hash-count-rothman $wtp $a$b | \
+            ./hash-count-rothman $wtp $a$b longest-timeseries.tsv | \
             gzip | \
             aws s3 cp - "$fname_out"
+
+        break
+
     done
   done
 done
--- kmer-distribution-pipeline.sh
+++ kmer-distribution-pipeline.sh
@@ -2,5 +2,5 @@ echo "Determining what inputs to consider..."
 ./find-best-timeseries.sh
 echo "Trimming adapters..."
 ./trim-consistent-timeseries.sh
-#echo "Counting kmers..."
-#./count-consistent-timeseries.sh
+echo "Counting kmers..."
+./count-consistent-timeseries.sh
