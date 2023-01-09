8a0ecacd54c5fd3915c8a1930e98bae64a35e311
Jeff Kaufman <jeff@securebio.org>
2023-01-09 15:59:49 +0000

    compute-mean-and-variance: uniq the output
    this has some '.gz' files that are actually plain text -- fix if we do more of this

--- compute-mean-and-variance.sh
+++ compute-mean-and-variance.sh
@@ -5,3 +5,22 @@ for wtp in $(cat longest-timeseries.tsv | awk '{print $3}' | sort | uniq); do
         echo "s3://prjna729801/clean-TS-$wtp-$a.gz"
     done
 done | xargs -P 32 -I {} ./compute-mean-and-variance-single.sh {}
+
+for wtp in $(cat longest-timeseries.tsv | awk '{print $3}' | sort | uniq); do
+    for a in A C G T; do
+        aws s3 cp "s3://prjna729801/clean-TS-$wtp-$a-mv.gz" - | \
+            uniq | \
+            gzip | \
+            aws s3 cp - "s3://prjna729801/clean-TS-$wtp-$a-mv-uniq.gz"
+    done
+done
+
+for wtp in $(cat longest-timeseries.tsv | awk '{print $3}' | sort | uniq); do
+    cat <(aws s3 cp "s3://prjna729801/clean-TS-$wtp-A-mv-uniq.gz" -| gunzip) \
+        <(aws s3 cp "s3://prjna729801/clean-TS-$wtp-C-mv-uniq.gz" -| gunzip) \
+        <(aws s3 cp "s3://prjna729801/clean-TS-$wtp-G-mv-uniq.gz" -| gunzip) \
+        <(aws s3 cp "s3://prjna729801/clean-TS-$wtp-T-mv-uniq.gz" -| gunzip) | \
+        sort -n | \
+        uniq | \
+        aws s3 cp - "s3://prjna729801/clean-TS-$wtp-mv-uniq.gz"
+done
