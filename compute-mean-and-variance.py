6228c9dd95146ad459fdc3bb69c16dca0449d8c5
Jeff Kaufman <jeff@securebio.org>
2023-01-10 21:11:21 +0000

    was not actually computing variance -- should not be taking square root

--- compute-mean-and-variance.py
+++ compute-mean-and-variance.py
@@ -13,10 +13,10 @@ for line in sys.stdin:
 
     mean = sum(vals) / n_vals
     squared_errors = [(x - mean)*(x-mean) for x in vals]
-    rms_error = math.sqrt(sum(squared_errors) / n_vals)
+    unbiased_variance = sum(squared_errors) / (n_vals-1)
 
     log_mean = math.log(mean, 10)
-    counts["%.2f\t%.2f" % (log_mean, rms_error / mean)] += 1
+    counts["%.2f\t%.2f" % (log_mean, unbiased_variance / mean)] += 1
 
 for record, count in counts.items():
     print("%s\t%s" % (count, record))
