b355127bfeb2b178be575d2aae9dd3ae4cd1707e
Jeff Kaufman <jeff@securebio.org>
2023-01-17 16:13:26 +0000

    find-best-timeseries: also compute number of reads

--- find-best-timeseries.sh
+++ find-best-timeseries.sh
@@ -3,7 +3,8 @@
 if [ ! -e rothman.unenriched.lengths.simple ]; then
     cat rothman.unenriched.simple | \
         while read accession date wtp ; do
-            echo $accession $date $wtp $(
+            echo $accession $date $wtp \
+                 $(
                 aws s3 cp s3://prjna729801/${accession}_1.fastq.gz - \
                     2>/dev/null | \
                     gunzip | \
@@ -15,3 +16,15 @@ fi
 
 cat rothman.unenriched.lengths.simple | \
     ./find-best-timeseries.py > longest-timeseries.tsv
+
+cat longest-timeseries.tsv | \
+    awk '{print $1}' | \
+    xargs -P 32 -I {} bash -c \
+        "aws s3 cp s3://prjna729801/{}_1.fastq.gz - | \
+         gunzip | \
+         grep -c ^@ > {}.n_reads"
+
+cat longest-timeseries.tsv | \
+    while read accession date wtp ; do
+        echo $accession $date $wtp $(cat $accession.n_reads)
+    done | tr ' ' '\t' > longest-timeseries-with-lengths.tsv
