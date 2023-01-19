04f53a7b57568f0df21a7d86edd2aa1323b9b724
Jeff Kaufman <jeff@securebio.org>
2023-01-19 02:31:00 +0000

    compute-correlation: subsample

--- compute-correlation.sh
+++ compute-correlation.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+if [ ! -d /run/kmer ]; then
+    sudo mkdir /run/kmer
+    sudo chown $(whoami) /run/kmer
+fi
+
 for a in A C G T; do
     for wtp in $(cat longest-timeseries.tsv | awk '{print $3}' | sort | uniq); do
         echo $a $wtp
-        fname_in="clean-TS-$wtp-$a.gz"
-        fname_out="clean-TS-$wtp-$a.sorted.gz"
+        fname_in="clean-TS-ss-$wtp-$a.gz"
+        fname_out="clean-TS-ss-$wtp-$a.sorted.gz"
         aws s3 cp "s3://prjna729801/${fname_in}" /run/kmer/
         cat /run/kmer/$fname_in | \
             gunzip | \
@@ -17,19 +22,19 @@ for a in A C G T; do
     for b in A C G T; do
         for c in A C G T; do
             prefix="$a$b$c"
-            fname_out="clean-TS-$prefix-correlation.tsv"
+            fname_out="clean-TS-ss-$prefix-correlation.tsv"
             echo $prefix to $fname_out
             ./compute-correlation.py \
                 tmp.$prefix.progress \
                 $prefix \
-                /run/kmer/clean-TS-*-$a.sorted.gz | \
+                /run/kmer/clean-TS-ss-*-$a.sorted.gz | \
                 aws s3 cp - "s3://prjna729801/${fname_out}" &
         done
     done
     wait
 
     echo "saving and cleaning up $a..."
-    for x in /run/kmer/clean-TS-*-$a.sorted.gz; do
+    for x in /run/kmer/clean-TS-ss-*-$a.sorted.gz; do
         aws s3 cp $x s3://prjna729801/ && rm $x
     done
 done
