2c23e8558f6c0eba413515c3506032fc987db645
Jeff Kaufman <jeff@securebio.org>
2023-01-20 03:40:23 +0000

    parallelize tbrv counting

--- count-reads-tbrv.sh
+++ count-reads-tbrv.sh
@@ -3,9 +3,17 @@
 METADATA=longest-timeseries-with-lengths.tsv
 OUT="longest-timeseries-tbrv.tsv"
 SLUG=tbrv
+
+for accession in $(cat $METADATA | awk '{print $1}'); do
+    aws s3 cp s3://prjna729801/$accession.$SLUG.fasta.gz - | \
+          gunzip | \
+          grep -c "^>" > $SLUG.$accession.count &
+    echo $accession
+done
+
+wait
+
 cat $METADATA | while read accession date wtp total_reads; do
     echo $accession $date $wtp $total_reads \
-         $(aws s3 cp s3://prjna729801/SRR14530892.$SLUG.fasta.gz - | \
-               gunzip | \
-               grep -c "^>")
+         $(cat $SLUG.$accession.count)
 done | tr ' ' '\t' > $OUT
