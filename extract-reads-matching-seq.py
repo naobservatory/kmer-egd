029314401f1e640585c65e895ed4d403a07daf6e
Jeff Kaufman <jeff@securebio.org>
2023-01-20 03:20:59 +0000

    scripts for extracting reads matching at least one 40-mer of a sequence

---
+++ extract-reads-matching-seq.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+from Bio.SeqIO.QualityIO import FastqGeneralIterator
+
+seq_fname, = sys.argv[1:]
+K=40
+to_match = set()
+
+def rc(s):
+    return "".join({'T':'A',
+                    'G':'C',
+                    'A':'T',
+                    'C':'G',
+                    'N':'N'}[x] for x in reversed(s))
+
+with open(seq_fname) as inf:
+    seq, = inf
+    seq = seq.strip()
+    for i in range(len(seq) - K + 1):
+        kmer_in = seq[i:i+K]
+        for kmer in [kmer_in, rc(kmer_in)]:
+            to_match.add(kmer)
+
+for title, seq, quality in FastqGeneralIterator(sys.stdin):
+    for i in range(len(seq) - K + 1):
+        kmer = seq[i:i+K]
+        if kmer in to_match:
+            print(">%s\n%s" % (title, seq))
+                
+    
---
+++ extract-reads-matching-seq.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+SEQ=tomato.brown.rugose.nt.seq
+METADATA=longest-timeseries.tsv
+SLUG=tbrv
+
+for accession in $(cat $METADATA | awk '{print $1}'); do
+    aws s3 cp s3://prjna729801/$accession.arclean.fastq.gz - | \
+        gunzip | \
+        ./extract-reads-matching-seq.py $SEQ | \
+        gzip | \
+        aws s3 cp - s3://prjna729801/$accession.$SLUG.fasta.gz &
+done
+wait
