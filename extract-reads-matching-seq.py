2e705c1fcd6c772169789ae99ba2d5f0a432d163
Jeff Kaufman <jeff@securebio.org>
2023-01-27 17:35:23 +0000

    extract-reads-matching-seq: support fasta input, allow passing args on command line

--- extract-reads-matching-seq.py
+++ extract-reads-matching-seq.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+from Bio.SeqIO.FastaIO import SimpleFastaParser
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
 
 seq_fname, = sys.argv[1:]
@@ -14,19 +15,25 @@ def rc(s):
                     'C':'G',
                     'N':'N'}[x] for x in reversed(s))
 
-with open(seq_fname) as inf:
-    seq, = inf
-    seq = seq.strip()
-    for i in range(len(seq) - K + 1):
-        kmer_in = seq[i:i+K]
-        for kmer in [kmer_in, rc(kmer_in)]:
-            to_match.add(kmer)
+if seq_fname.endswith(".fasta"):
+    with open(seq_fname) as inf:
+        (_, seq), = SimpleFastaParser(inf)
+else:
+    with open(seq_fname) as inf:
+        seq, = inf
+
+seq = seq.strip()
+for i in range(len(seq) - K + 1):
+    kmer_in = seq[i:i+K]
+    for kmer in [kmer_in, rc(kmer_in)]:
+        to_match.add(kmer)
 
 for title, seq, quality in FastqGeneralIterator(sys.stdin):
     for i in range(len(seq) - K + 1):
         kmer = seq[i:i+K]
         if kmer in to_match:
             print(">%s\n%s" % (title, seq))
+            # only extract the read once
             break
                 
     
--- extract-reads-matching-seq.sh
+++ extract-reads-matching-seq.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-SEQ=pmmov.seq
+FASTA=$1
 METADATA=longest-timeseries.tsv
-SLUG=pmmov
+SLUG=reads-${FASTA/.fasta/}
 
 mkdir $SLUG
 
 for accession in $(cat $METADATA | awk '{print $1}'); do
     aws s3 cp s3://prjna729801/${accession}.arclean.fastq.gz - | \
         gunzip | \
-        ./extract-reads-matching-seq.py $SEQ \
+        ./extract-reads-matching-seq.py $FASTA \
            > $SLUG/$accession.fasta &
 done
 wait
 
 zip -r $SLUG.zip $SLUG/
+rm -r $SLUG/
